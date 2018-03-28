189645583888322c2ee6149f469c934f52ec3e54
Kevin Rosaria <kevinrosaria01@gmail.com>
2018-03-28 16:22:44 +0800

    history log file and detail page

--- .cradle.php
+++ .cradle.php
@@ -16,71 +16,119 @@ $this->addLogger(function($message, $request, $response) {
     $logRequest = Request::i()->load();
     $logResponse = Response::i()->load();
 
-    //get data
-    $data = [];
-    $data['results'] = $response->hasResults() ? $response->getResults() : '';
-    $data['stage'] = $request->hasStage() ? $request->getStage() : '';
-    $data['post'] = $request->hasPost() ? $request->getPost() : '';
-    $body = !empty($request->getBody()) ? $request->getBody(): '';
-
-    if (strlen($body) > 300) {
-        $body = '< DATA TOO LONG >';
-    }
-
-    //case for long string and data
-    foreach ($data as $keyword => $result) {
-        if (is_array($result)) {
-            foreach ($result as $key => $row) {
-                if (is_array($row)) {
-                    if (count($row) > 100) {
-                        $data[$keyword][$key] = '< DATA TOO LONG >';
-                        continue;
-                    }
-
-                    foreach ($row as $id => $value) {
-                        if (is_array($value)) {
-                            $value = json_encode($value);
-                        }
-
-                        if (strlen($value) > 300) {
-                            $data[$keyword][$key][$id] = '< DATA TOO LONG >';
-                        }
-                    }
-
-                    continue;
-                }
-
-                if (strlen($row) > 300) {
-                    $data[$keyword][$key] = '< DATA TOO LONG >';
-                }
-            }
-
-            continue;
-        }
-
-        if (strlen($result) > 300) {
-            $data[$keyword] = '< DATA TOO LONG >';
-        }
-    }
-
-    //then reset
-    $request->setStage($data['stage']);
-    $request->setPost($data['post']);
-    $request->setBody($body);
-    $response->setError(false)->setResults($data['results']);
-
     //record logs
     $logRequest
         ->setStage('history_remote_address', $request->getServer('REMOTE_ADDR'))
         ->setStage('profile_id', $request->getSession('me', 'profile_id'))
         ->setStage('history_page', $request->getServer('REQUEST_URI'))
         ->setStage('history_activity', $message)
-        ->setStage('history_flag', 0)
-        ->setStage('history_meta', [
+        ->setStage('history_flag', 0);
+
+    $config = $this->package('global')->config('settings');
+
+    $meta = [];
+    if (isset($config['history']['file']) && $config['history']['file']) {
+        //To Enable
+        //Insert a new settings for history
+        // Ex: 'history' => [
+        //      'file' => true,
+        //      'path' => 'some/path/under/config'
+        //      ];
+        // Note: Make sure folder permission is 777 of the folders
+
+        //generate uniq file name
+        $filename = strtotime(date('Y-m-d h:i:s')) + rand();
+
+        //change if history path is empty
+        if (empty($config['history']['path'])) {
+            $config['history']['path'] = 'log';
+        }
+
+        $path = $this->package('global')->path('config') . '/' . $config['history']['path'];
+
+        //if not existing create the folder
+        if (!is_dir($path)) {
+            mkdir($path, 0777);
+        }
+
+        $meta = [
             'request' => $request->get(),
             'response' => $response->get(),
-        ]);
+        ];
 
+        file_put_contents(
+            $path . '/' . $filename . '.json',
+            json_encode($meta)
+        );
+
+        //set the new meta
+        $meta = [
+            'log_file' => sprintf('%s/%s.json', $config['history']['path'], $filename)
+        ];
+    }
+
+    //if history setting is not set (default)
+    if (!isset($meta['log_file'])) {
+        //get meta
+        $data['results'] = $response->hasResults() ? $response->getResults() : '';
+        $data['stage'] = $request->hasStage() ? $request->getStage() : '';
+        $data['post'] = $request->hasPost() ? $request->getPost() : '';
+        $body = !empty($request->getBody()) ? $request->getBody(): '';
+
+        if (strlen($body) > 300) {
+            $body = '< DATA TOO LONG >';
+        }
+
+        //case for long string and data
+        foreach ($data as $keyword => $result) {
+            if (is_array($result)) {
+                foreach ($result as $key => $row) {
+                    if (is_array($row)) {
+                        if (count($row) > 100) {
+                            $data[$keyword][$key] = '< DATA TOO LONG >';
+                            continue;
+                        }
+
+                        foreach ($row as $id => $value) {
+                            if (is_array($value)) {
+                                $value = json_encode($value);
+                            }
+
+                            if (strlen($value) > 300) {
+                                $data[$keyword][$key][$id] = '< DATA TOO LONG >';
+                            }
+                        }
+
+                        continue;
+                    }
+
+                    if (strlen($row) > 300) {
+                        $data[$keyword][$key] = '< DATA TOO LONG >';
+                    }
+                }
+
+                continue;
+            }
+
+            if (strlen($result) > 300) {
+                $data[$keyword] = '< DATA TOO LONG >';
+            }
+        }
+
+        //then reset
+        $request->setStage($data['stage']);
+        $request->setPost($data['post']);
+        $request->setBody($body);
+        $response->setError(false)->setResults($data['results']);
+
+        $meta = [
+            'request' => $request->get(),
+            'response' => $response->get(),
+        ];
+    }
+
+    //record logs
+    $logRequest->setStage('history_meta', $meta);
     $this->trigger('history-create', $logRequest, $logResponse);
 });
 
--- src/controller.php
+++ src/controller.php
@@ -132,6 +132,149 @@ $this->get('/admin/history/search', function ($request, $response) {
 });
 
 /**
+ * Render the System Model Update Page
+ *
+ * @param Request $request
+ * @param Response $response
+ */
+$this->get('/admin/history/detail/:history_id', function ($request, $response) {
+    //----------------------------//
+    // 1. Prepare Data
+    //get schema data
+    $schema = Schema::i('history');
+
+    //pass the item with only the post data
+    $data = ['item' => $request->getPost()];
+
+    //also pass the schema to the template
+    $data['schema'] = $schema->getAll();
+
+    //if this is a return back from processing
+    //this form and it's because of an error
+    if ($response->isError()) {
+        //pass the error messages to the template
+        $response->setFlash($response->getMessage(), 'error');
+        $data['errors'] = $response->getValidation();
+    }
+
+    //get the original table row
+    $this->trigger('history-detail', $request, $response);
+
+    //can we update ?
+    if ($response->isError()) {
+        //redirect
+        $redirect = '/admin/history/search';
+
+        //this is for flexibility
+        if ($request->hasStage('redirect_uri')) {
+            $redirect = $request->getStage('redirect_uri');
+        }
+
+        //add a flash
+        $this->package('global')->flash($response->getMessage(), 'error');
+        return $this->package('global')->redirect($redirect);
+    }
+
+    $data['detail'] = $response->getResults();
+
+    //if no item
+    if (empty($data['item'])) {
+        //pass the item to the template
+        $data['item'] = $data['detail'];
+
+        if (is_array($data['item']['history_meta'])) {
+            $data['item']['history_meta'] = json_encode($data['item']['history_meta'], JSON_PRETTY_PRINT);
+        }
+
+        //add suggestion value for each relation
+        foreach ($data['schema']['relations'] as $name => $relation) {
+            if ($relation['many'] > 1) {
+                continue;
+            }
+
+            $suggestion = '_' . $relation['primary2'];
+
+            $suggestionData = $data['item'];
+            if ($relation['many'] == 0) {
+                if (!isset($data['item'][$relation['name']])) {
+                    continue;
+                }
+
+                $suggestionData = $data['item'][$relation['name']];
+
+                if (!$suggestionData) {
+                    continue;
+                }
+            }
+
+            try {
+                $data['item'][$suggestion] = Schema::i($relation['name'])
+                    ->getSuggestionFormat($suggestionData);
+            } catch (Exception $e) {
+            }
+        }
+    }
+
+    //if we only want the raw data
+    if ($request->getStage('render') === 'false') {
+        return;
+    }
+
+    //determine the suggestion
+    $data['detail']['suggestion'] = $schema->getSuggestionFormat($data['item']);
+
+    //add CSRF
+    $this->trigger('csrf-load', $request, $response);
+    $data['csrf'] = $response->getResults('csrf');
+
+    //if there are file fields
+    if (!empty($data['schema']['files'])) {
+        //add CDN
+        $config = $this->package('global')->service('s3-main');
+        $data['cdn_config'] = File::getS3Client($config);
+    }
+
+    //determine valid relations
+    $data['valid_relations'] = [];
+    $this->trigger('system-schema-search', $request, $response);
+    foreach ($response->getResults('rows') as $relation) {
+        $data['valid_relations'][] = $relation['name'];
+    }
+
+    $data['redirect'] = urlencode($request->getServer('REQUEST_URI'));
+
+    //----------------------------//
+    // 2. Render Template
+    //set the class name
+    $class = 'page-admin-history-detail page-admin';
+
+    //determine the title
+    $data['title'] = $this->package('global')->translate('Viewing history #%s', $request->getStage('history_id'));
+
+    //render the body
+    $body = $this
+        ->package('cradlephp/cradle-history')
+        ->template('detail', $data, [
+            'detail_detail',
+            'detail_format'
+        ]);
+
+    //set content
+    $response
+        ->setPage('title', $data['title'])
+        ->setPage('class', $class)
+        ->setContent($body);
+
+    //if we only want the body
+    if ($request->getStage('render') === 'body') {
+        return;
+    }
+
+    //render page
+    $this->trigger('admin-render-page', $request, $response);
+});
+
+/**
  * Renders an update form
  *
  * @param Request $request
